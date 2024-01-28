a413b3e953d8104e4aa7ac463e98f843ae253845
mayank negi <mayank.negi0@gmail.com>
2024-01-28 17:58:59 +0530

    disabled cors

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const todoRouter = require("./routes/todoRouter");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors);
+// app.use(cors);
 app.use(express.json());
 setupDB();
 app.use("/user", userRouter);
