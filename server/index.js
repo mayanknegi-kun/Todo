3ee31ffea5ad6b3bf0868343330c6d4c04b28f24
mayank negi <mayank.negi0@gmail.com>
2024-01-28 17:36:04 +0530

    chore: port env

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const setupDB = require("./utils/db");
 const userRouter = require("./routes/userRouter");
 const todoRouter = require("./routes/todoRouter");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// app.use(cors);
+app.use(cors);
 app.use(express.json());
 setupDB();
 app.use("/user", userRouter);
