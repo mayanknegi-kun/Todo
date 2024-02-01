ae1b8f1e171121f8a69dee7df604c9b585b6fbbc
mayank negi <mayank.negi0@gmail.com>
2024-02-02 03:02:29 +0530

    home page css

--- client/src/api/apiHandler.js
+++ client/src/api/apiHandler.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { endPoints } from "./endpint";
 
-export const login = async (request) => {
+export const triggerLogin = async (request) => {
   const [error, response] = await axios.post(endPoints.login, request);
   if (error) Promise.reject(error);
   if (response) Promise.resolve(response);
 };
 
-export const signup = async (request) => {
+export const triggerSignup = async (request) => {
   try {
     const response = await axios.post(endPoints.signup, request);
     return Promise.resolve(response?.data);
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import { signup } from "../api/apiHandler";
+import { triggerLogin, triggerSignup } from "../api/apiHandler";
 import { updateUserData } from "../utils/localStorage";
 import { useState } from "react";
 
 const Home = () => {
+  const [signup, setSignup] = useState(true);
   const [inputData, setInputData] = useState({
     username: "",
     email: "",
@@ -20,50 +19,127 @@ const Home = () => {
     }));
   };
 
-  const handleLogin = () => {
-    signup(inputData)
-      .then((res) => {
-        updateUserData({ authToken: res.authToken });
-        setInputData({
-          username: "",
-          email: "",
-          password: "",
-        });
-      })
-      .catch((e) => console.log(e));
+  const handleAuth = async () => {
+    try {
+      let response;
+      if (signup) {
+        response = await triggerSignup(inputData);
+      } else {
+        response = await triggerLogin(inputData);
+      }
+      updateUserData({ authToken: response.authToken });
+      setInputData({
+        username: "",
+        email: "",
+        password: "",
+      });
+    } catch (err) {
+      console.log("error", err);
+    }
   };
-  console.log();
-  return (
-    <>
-      <div className="text-3xl ">Signup</div>
-      <div className=" m-auto grid w-1/2 gap-3">
-        <TextField
-          id="username"
-          label="Username"
-          variant="standard"
-          value={inputData?.username}
-          onChange={handleChange}
-        />
-        <TextField
-          id="email"
-          label="Email"
-          variant="standard"
-          value={inputData?.email}
-          onChange={handleChange}
-        />
-        <TextField
-          id="password"
-          label="Password"
-          variant="standard"
-          type="password"
-          value={inputData?.password}
-          onChange={handleChange}
-        />
-        <Button variant="outlined" onClick={handleLogin}>
-          Signup
-        </Button>
+
+  const toggleAuthOption = (signup) => {
+    setSignup(signup);
+  };
+
+  const formContent = ({ signup }) => {
+    return (
+      <div className="flex gap-8 h-full">
+        <div
+          className={`flex aligb-center justify-center p-8 h-full w-1/2 bg-[#FFFFFF] order-${
+            signup ? "1" : "2"
+          }`}
+        >
+          {signup ? (
+            <p className="text-[#FF5631] font-light">
+              Welcome aboard, New Friend!. Congratulations on taking the first
+              step towards a more organized and productive life. Get ready to
+              unlock your full potential as we embark on this journey together.
+              From setting goals to crushing them, our todo app is here to
+              support you every step of the way. Let's start building your
+              roadmap to success!
+            </p>
+          ) : (
+            <p className="text-[#FF5631] font-light">
+              Welcome back. Your todos await, ready to be organized,
+              prioritized, and conquered. Let's dive in and make today a
+              success.
+            </p>
+          )}
+        </div>
+        <div className={`flex flex-col p-10 w-1/2 order-${signup ? "2" : "1"}`}>
+          <label htmlFor="username" className="text-[#FF5631] pb-3">
+            Username:
+          </label>
+          <input
+            id="username"
+            label="Username"
+            className="rounded h-8 mb-4 w-3/4"
+            value={inputData?.username}
+            onChange={handleChange}
+          />
+          {signup && (
+            <>
+              <label htmlFor="username" className="text-[#FF5631] pb-3">
+                Email:
+              </label>
+              <input
+                id="email"
+                label="Email"
+                className="rounded h-8 mb-4 w-3/4"
+                value={inputData?.email}
+                onChange={handleChange}
+              />
+            </>
+          )}
+          <label htmlFor="username" className="text-[#FF5631] pb-3">
+            Password:
+          </label>
+          <input
+            id="password"
+            label="Password"
+            type="password"
+            className="rounded h-8 mb-8 w-3/4"
+            value={inputData?.password}
+            onChange={handleChange}
+          />
+          <button
+            className="bg-[#FF5631] rounded h-8 mb-8 w-3/4"
+            onClick={handleAuth}
+          >
+            {signup ? "SIGN UP" : "Login"}
+          </button>
+        </div>
       </div>
-    </>
+    );
+  };
+
+  return (
+    <div className="h-screen flex justify-center align-center bg-[#CEBEA4]">
+      <div className="h-4/5 w-4/5 pt-20">
+        <div className="flex gap-10 mb-10">
+          <button
+            className={`font-sans font-medium text-lg text-[${
+              signup ? "#FF5631" : "#FFFFFF"
+            }]  hover:text-[#FF5631]`}
+            onClick={() => toggleAuthOption(true)}
+          >
+            SIGN UP
+          </button>
+          <button
+            className={`font-sans font-medium text-lg text-[${
+              !signup ? "#FF5631" : "#FFFFFF"
+            }] hover:text-[#FF5631]`}
+            onClick={() => toggleAuthOption(false)}
+          >
+            LOGIN
+          </button>
+        </div>
+        <div className="flex flex-col h-full bg-[#0D0D0D]">
+          {formContent({ signup: signup })}
+        </div>
+      </div>
+    </div>
   );
 };
 
